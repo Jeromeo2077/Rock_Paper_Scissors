33a09ec54261acfba0be54308232b75e4f74cb2e
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-14 15:24:28 -0600

    Refactor assembly version in AssemblyInfo.cs

--- obj/Debug/net8.0/Rock_Paper_Scissors.AssemblyInfo.cs
+++ obj/Debug/net8.0/Rock_Paper_Scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f9e6a213efa81ae24a9b73d8917bfa73349621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f0c804c7bdf7cf1b47a105ba53dba9aaff82d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
