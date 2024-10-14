145bff068bfd24b32b5637b43b04cf8bd9cfcba7
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-14 16:18:01 -0600

    Refactor console output message to display user's and computer's running score and enabled the ability to continue playing

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿internal class Program
 {
+
+  static int UserScore = 0;
+  static int ComputerScore = 0;
   private static void Main()
   {
     Console.WriteLine("Welcome to Rock, Paper, and Scissors!!!");
@@ -18,61 +21,81 @@
     else if (userHand == "Rock" && computerHand == "Scissors" || userHand == "Paper" && computerHand == "Rock" || userHand == "Scissors" && computerHand == "Paper")
     {
       Console.WriteLine("You win!");
+      UserScore++;
     }
     else
     {
       Console.WriteLine("Computer wins!");
+      ComputerScore++;
     }
-  }
 
-  static string GetUserHand()
-  {
-    Console.WriteLine("Please enter your choice: ");
-    Console.WriteLine("1. Rock");
-    Console.WriteLine("2. Paper");
-    Console.WriteLine("3. Scissors");
+    Console.WriteLine();
+    Console.WriteLine($"User Score: {UserScore}");
+    Console.WriteLine($"Computer Score: {ComputerScore}");
+    Console.WriteLine();
 
-    string? userHand = Console.ReadLine();
-
-    if (userHand == "1")
+    Console.WriteLine("Would you like to play again? (Y/N)");
+    string playAgain = Console.ReadLine();
+    if (playAgain == "Y" || playAgain == "y")
     {
-      userHand = "Rock";
-      return userHand;
-    }
-    else if (userHand == "2")
-    {
-      userHand = "Paper";
-      return userHand;
-    }
-    else if (userHand == "3")
-    {
-      userHand = "Scissors";
-      return userHand;
-    }
-    else  // If the user enters an invalid input
-    {
-      Console.WriteLine();
-      Console.WriteLine("Invalid input. Please try again.");
-      Console.WriteLine();
-      return GetUserHand();
-    }
-  }
-
-  static string GetComputerHand()
-  {
-    int computerHand = new Random().Next(1, 4);
-
-    if (computerHand == 1)
-    {
-      return "Rock";
-    }
-    else if (computerHand == 2)
-    {
-      return "Paper";
+      Main();
     }
     else
     {
-      return "Scissors";
+      Console.WriteLine("Thank you for playing!");
+      Console.WriteLine($"User Score: {UserScore}");
+      Console.WriteLine($"Computer Score: {ComputerScore}");
+    }
+
+    static string GetUserHand()
+    {
+      Console.WriteLine("Please enter your choice: ");
+      Console.WriteLine("1. Rock");
+      Console.WriteLine("2. Paper");
+      Console.WriteLine("3. Scissors");
+
+      string? userHand = Console.ReadLine();
+
+      if (userHand == "1")
+      {
+        userHand = "Rock";
+        return userHand;
+      }
+      else if (userHand == "2")
+      {
+        userHand = "Paper";
+        return userHand;
+      }
+      else if (userHand == "3")
+      {
+        userHand = "Scissors";
+        return userHand;
+      }
+      else  // If the user enters an invalid input
+      {
+        Console.WriteLine();
+        Console.WriteLine("Invalid input. Please try again.");
+        Console.WriteLine();
+        return GetUserHand();
+      }
+    }
+
+    static string GetComputerHand()
+    {
+      int computerHand = new Random().Next(1, 4);
+
+      if (computerHand == 1)
+      {
+        return "Rock";
+      }
+      else if (computerHand == 2)
+      {
+        return "Paper";
+      }
+      else
+      {
+        return "Scissors";
+      }
     }
   }
 }
--- obj/Debug/net8.0/Rock_Paper_Scissors.AssemblyInfo.cs
+++ obj/Debug/net8.0/Rock_Paper_Scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a09ec54261acfba0be54308232b75e4f74cb2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83ea55ec11a561f098c6b03f9f9202bfb5395c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
